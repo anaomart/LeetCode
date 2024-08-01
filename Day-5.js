28f5867b99cc51b6556ae2d66b6ea19bf3339a9a
anaomart <anaomart@gmail.com>
2024-08-01 17:57:56 +0300

    day-5 - easy

--- Day-5.js
+++ Day-5.js
@@ -1,34 +1,13 @@
-var canPlaceFlowers = function(flowerbed, n) {
-    // let counter = 0
-    // for(let i =0 ; i <flowerbed.length ; i++){
-    //     if(flowerbed[i] + flowerbed[i-1] + flowerbed[i+1] === 0){
-    //         counter++
-    //     }
-    // }
-    // return counter >= n 
-
-    const insertOne = (array) => {
-        for (let i = 0; i < flowerbed.length; i++) {
-            if (flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] === 0) {
-                array[i] = 1
-                counter++
-                break
-            }
-            return counter
+var canPlaceFlowers = function(array, n) {
+    let counter = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (!!array[i - 1] + !!array[i] + !!array[i + 1] === 0) {
+            array[i] = 1;
+            counter++;
         }
-        console.log(counter)
-        insertOne(array)
-
-
     }
-
-
-
-
-
-
-
+    return counter >= n;
 };
-const array = [1, 0, 0, 0, 0, 1]
+const array = [0, 0, 1, 0, 1];
 
-console.log(canPlaceFlowers(array, 1))
+console.log(canPlaceFlowers(array, 1));
