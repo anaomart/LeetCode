9d325830ca5c512f1aee7c437f5e329a12e4dc65
anaomart <anaomart@gmail.com>
2024-12-10 09:57:37 +0200

    'day7'

--- New Start/Day-3/206. Reverse Linked List.ts
+++ New Start/Day-3/206. Reverse Linked List.ts
@@ -1,12 +1,5 @@
 
-  class ListNode {
-     val: number
-      next: ListNode | null
-     constructor(val?: number, next?: ListNode | null) {
-         this.val = (val===undefined ? 0 : val)
-          this.next = (next===undefined ? null : next)
-      }
-  }
+
  
 
 function reverseList(head: ListNode | null): ListNode | null {
